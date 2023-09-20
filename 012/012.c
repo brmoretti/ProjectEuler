3e0a90e130d961e5c801e110ab6fcd854f3c9483
Bruno Moretti <brmoretti@gmail.com>
2023-09-19 23:02:07 -0300

    012

---
+++ 012/012.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+unsigned long long int	ft_n_divisors(unsigned long long int nb)
+{
+	int						n_divisors;
+	unsigned long long int	i;
+	int						increment;
+	unsigned long long int	half_nb;
+
+	n_divisors = 2;
+	i = 1;
+	increment = 1;
+	half_nb = nb / 2;
+	if (nb > 2 && nb % 2 != 0)
+		increment = 2;
+	while (i < half_nb)
+	{
+		if (nb % i == 0)
+			n_divisors++;
+		i += increment;
+	}
+	return (n_divisors);
+}
+
+void	ft_next_even_triangle(unsigned long long int *i,
+						unsigned long long int *triangle)
+{
+	(*i)++;
+	*triangle += *i;
+	if (*triangle % 2 != 0)
+		ft_next_even_triangle(i, triangle);
+}
+
+int	main(void)
+{
+	unsigned long long int	i;
+	unsigned long long int	triangle;
+	unsigned long long int	n_divisors;
+
+	i = 1;
+	triangle = 1;
+	n_divisors = 1;
+	while (ft_n_divisors(triangle) <= 500)
+		ft_next_even_triangle(&i, &triangle);
+	printf("%d\n", triangle);
+	return (0);
+}
